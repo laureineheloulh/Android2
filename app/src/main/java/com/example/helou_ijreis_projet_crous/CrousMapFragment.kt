e7954837324f6b1f3227e432efc968e29f1d3861
hadi-ijreis <hadiijreis@gmail.com>
2021-11-08 15:11:57 +0100

    Added google maps implementation

--- app/src/main/java/com/example/helou_ijreis_projet_crous/CrousMapFragment.kt
+++ app/src/main/java/com/example/helou_ijreis_projet_crous/CrousMapFragment.kt
@@ -5,25 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 
+const val MAP_CROUS = "crous_map"
+
 class CrousMapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mView: View
+    private lateinit var mapView: MapView
     private lateinit var crousShelf: ArrayList<Crous>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       /* val supportMapFragment: SupportMapFragment = SupportMapFragment.newInstance()
+        val argCrous = requireArguments().getSerializable(MAP_CROUS) as ArrayList<Crous>?
+        crousShelf = argCrous ?: ArrayList()
+        val supportMapFragment: SupportMapFragment = SupportMapFragment.newInstance()
         supportMapFragment.getMapAsync(this)
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.map_layout, supportMapFragment)
-        fragmentTransaction.commit()*/
 
     }
 
@@ -32,22 +33,39 @@ class CrousMapFragment : Fragment(), OnMapReadyCallback {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crous_map, container, false)
+        crousShelf = requireArguments().getSerializable(MAP_CROUS) as ArrayList<Crous>
+        mView = inflater.inflate(R.layout.fragment_crous_map, container, false)
+        return mView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView = mView.findViewById(R.id.mapView)
+        if(mapView != null){
+            mapView.onCreate(null)
+            mapView.onResume()
+            mapView.getMapAsync(this)
+        }
     }
 
     companion object {
 
         @JvmStatic
-        fun newInstance(): CrousMapFragment =
-            CrousMapFragment()
+        fun newInstance(crous: ArrayList<Crous>) =
+            CrousMapFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(ARG_CROUS, crous)
 
+                }
+            }
     }
 
     override fun onMapReady(gmap: GoogleMap) {
         for(i in 0 until crousShelf.size){
         val location : LatLng = LatLng(crousShelf[i].latitude, crousShelf[i].longitude)
-        gmap.addMarker(
-            MarkerOptions()
+            val markerOptions = MarkerOptions()
+            gmap.addMarker(markerOptions
                 .position(location)
                 .title(crousShelf[i].name)
                 .snippet(crousShelf[i].type)
